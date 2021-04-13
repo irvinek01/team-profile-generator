0d60b47a0fbaa786f959aedf99e87015e7c9e95a
Arvin John Bautista <arvinyoung01@gmail.com>
2021-04-13 10:01:45 -0700

    Employee test and class added

---
+++ _tests_/Employee.test.js
@@ -0,0 +1,55 @@
+const { describe } = require("yargs");
+const Employee = require("../lib/Employee");
+
+const name = "John Doe";
+const id = 1;
+const email = "email@email.com";
+
+describe('Employee', () => {
+    describe('Initialization', () => {
+
+        it("object Employee should have the following properties: name, id, email, and role.", () => {
+            const objEmp = new Employee(name,id,email);
+            
+            expect(objEmp.name).toEqual(name);
+            expect(objEmp.id).toEqual(id);
+            expect(objEmp.email).toEqual(email);
+            expect(objEmp.role).toEqual("Employee");
+        });
+        
+
+    });
+
+    describe("getName", () => {
+		it("object Employee name should be returned.", () => {
+			const objEmp = new Employee(name,id,email);
+			const objEmpName = objEmp.getName();
+			expect(objEmpName).toBe(name);
+		});
+	});
+
+    describe("getId", () => {
+		it("object Employee id should be returned.", () => {
+			const objEmp = new Employee(name,id,email);
+			const objEmpId = objEmp.getId();
+			expect(objEmpId).toBe(id);
+		});
+	});
+
+    describe("getEmail", () => {
+		it("object Employee email should be returned.", () => {
+			const objEmp = new Employee(name,id,email);
+			const objEmpEmail = objEmp.getEmail();
+			expect(objEmpEmail).toBe(email);
+		});
+	});
+
+    describe("getRole", () => {
+		it("object Employee role should be returned.", () => {
+			const objEmp = new Employee(name,id,email);
+			const objEmpRole = objEmp.getRole();
+			expect(objEmpRole).toEqual("Employee");
+		});
+	});
+
+});
